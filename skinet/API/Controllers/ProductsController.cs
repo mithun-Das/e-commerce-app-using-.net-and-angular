8b4687ca27ccdb64128860258fc4f6989c8bf2a5
mithun-Das <mithun.das227@gmail.com>
2022-12-27 21:05:06 +0600

    Section-2: Part-10 -> Adding our first API Controller

---
+++ skinet/API/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    #region[GET Requests]
+
+    /// <Summary>
+    /// Get The Product List
+    /// </Summary>
+    [HttpGet]
+    public string GetProducts()
+    {
+        return "Product Lists";
+    }
+
+    /// <Summary>
+    /// Get The Specific Product
+    /// </Summary>
+    /// <param name="id"></param>
+    /// <returns> Product with specific id </returns>
+    [HttpGet("{id}")]
+    public string GetProduct(int id)
+    {
+        return "Single Product";
+    }
+
+    #endregion
+}
