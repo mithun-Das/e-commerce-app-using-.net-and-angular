2260468682bab391faea3bb3c24a38d14a4168de
mithun-Das <mithun.das227@gmail.com>
2022-12-28 00:14:44 +0600

    Section-2: Part-13 -> Adding a connection string

--- skinet/API/Program.cs
+++ skinet/API/Program.cs
@@ -1,4 +1,7 @@
+using API.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -10,6 +13,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>
+    (x => x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
