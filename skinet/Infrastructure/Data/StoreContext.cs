4e87f5f5c18bca23f172e67d5501772ba9f4b8ae
mithun-Das <mithun.das227@gmail.com>
2022-12-29 21:20:06 +0600

    Section-3: Part-25 -> Extending the products entity and creating related entities

--- skinet/Infrastructure/Data/StoreContext.cs
+++ skinet/Infrastructure/Data/StoreContext.cs
@@ -10,4 +10,6 @@ public class StoreContext : DbContext
     }
 
     public DbSet<Product> Products { get; set; }
+    public DbSet<ProductType> ProductTypes { get; set; }
+    public DbSet<ProductBrand> ProductBrands { get; set; }
 }
