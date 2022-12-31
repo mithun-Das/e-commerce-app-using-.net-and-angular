a447570549ceeedc9b019cb450a6c4db205c02cc
mithun-Das <mithun.das227@gmail.com>
2022-12-31 12:27:03 +0600

    Section-3: Part-29 -> Adding Seed data

--- skinet/Infrastructure/Data/ProductRepository.cs
+++ skinet/Infrastructure/Data/ProductRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Core.Entities;
 using Core.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Infrastructure.Data;
 
