c856d38cf918204a44ed8d9c528dada8b94be12c
mithun-Das <mithun.das227@gmail.com>
2023-01-11 15:26:07 +0600

    Section-5: Part-50 -> Creating a test controller for errors

---
+++ skinet/API/Controllers/BuggyController.cs
@@ -0,0 +1,48 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController : BaseApiController
+{
+    private readonly StoreContext _context;
+    public BuggyController(StoreContext context) 
+    { 
+        _context = context;
+    }
+
+    [HttpGet("notfound")]
+    public ActionResult GetNotFoundRequest()
+    {
+        var result = this._context.Products.Find(42);
+
+        if (result == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+
+    [HttpGet("servererror")]
+    public ActionResult GetServerError()
+    {
+        var result = this._context.Products.Find(42);
+
+        var resultToReturn = result.ToString();
+
+        return Ok();
+    }
+
+    [HttpGet("badrequest")]
+    public ActionResult GetBadRequest()
+    {
+        return BadRequest();
+    }
+
+    [HttpGet("badrequest/{id}")]
+    public ActionResult GetNotFoundRequest(int id)
+    {
+        return Ok();
+    }
+}
--- skinet/API/Controllers/ProductsController.cs
+++ skinet/API/Controllers/ProductsController.cs
@@ -11,9 +11,7 @@ using AutoMapper;
 
 namespace API.Controllers;
 
-[Route("api/[controller]")]
-[ApiController]
-public class ProductsController : ControllerBase
+public class ProductsController : BaseApiController
 {
     private readonly IGenericRepository<Product> _productsRepo;
     private readonly IGenericRepository<ProductBrand> _productBrandRepo;
