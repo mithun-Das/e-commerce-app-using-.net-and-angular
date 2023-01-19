efc8748162ddfd867e6d164baa16d0b69bddd232
mithun-Das <mithun.das227@gmail.com>
2023-01-19 10:33:03 +0600

    Section-9: Part-99 -> Adding the pagination functionality part 2

--- skinet/client/src/app/shop/shop.service.ts
+++ skinet/client/src/app/shop/shop.service.ts
@@ -4,6 +4,7 @@ import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import { map } from 'rxjs';
+import { ShopParams } from '../shared/models/shopParams';
 
 @Injectable({
   providedIn: 'root'
@@ -14,20 +15,20 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts(brandId?: number, typeId?: number, sort?: string) {
+  getProducts(shopParams: ShopParams) {
 
     let params = new HttpParams();
 
-    if (brandId) {
-      params = params.append('brandId', brandId);
+    if (shopParams.brandId) {
+      params = params.append('brandId', shopParams.brandId.toString());
     }
 
-    if (typeId) {
-      params = params.append('typeId', typeId);
+    if (shopParams.typeId) {
+      params = params.append('typeId', shopParams.typeId.toString());
     }
 
-    if(sort) {
-      params = params.append('sort', sort);
+    if(shopParams.sort) {
+      params = params.append('sort', shopParams.sort);
     }
 
     return this.http.get<IPagination>(this.baseUrl + 'products', { observe: 'response', params })
