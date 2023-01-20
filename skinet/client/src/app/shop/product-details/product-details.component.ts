d08d170eecdb9dc4d79be977e73d16e9bfc000be
mithun-Das <mithun.das227@gmail.com>
2023-01-20 09:57:49 +0600

    Section-10: Part-112 -> Getting an individual product

--- skinet/client/src/app/shop/product-details/product-details.component.ts
+++ skinet/client/src/app/shop/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: IProduct;
+
+  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(response => {
+      this.product = response;
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
