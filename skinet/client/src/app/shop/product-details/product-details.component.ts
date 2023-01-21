dc45c82e989f7e03555273a6c3d8a073fb06d307
mithun-Das <mithun.das227@gmail.com>
2023-01-21 17:57:42 +0600

    Section-12: Part-127 -> Using the breadcrumb service

--- skinet/client/src/app/shop/product-details/product-details.component.ts
+++ skinet/client/src/app/shop/product-details/product-details.component.ts
@@ -2,28 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
-
   product: IProduct;
 
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute) { }
+  constructor(
+    private shopService: ShopService,
+    private activateRoute: ActivatedRoute,
+    private bcService: BreadcrumbService
+  ) {}
 
   ngOnInit(): void {
     this.loadProduct();
   }
 
   loadProduct() {
-    this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(response => {
-      this.product = response;
-    }, error => {
-      console.log(error);
-    });
+    this.shopService
+      .getProduct(+this.activateRoute.snapshot.paramMap.get('id'))
+      .subscribe(
+        (response) => {
+          this.product = response;
+          this.bcService.set('@productDetails', this.product.name)
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
-
 }
