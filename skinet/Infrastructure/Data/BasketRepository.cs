90c5c39d70fa59dc2eb485dd69a20c7ccf4d0bc4
mithun-Das <mithun.das227@gmail.com>
2023-01-24 21:37:04 +0600

    Section-13: Part-139 -> Implementing the basket repository

--- skinet/Infrastructure/Data/BasketRepository.cs
+++ skinet/Infrastructure/Data/BasketRepository.cs
@@ -1,24 +1,43 @@
 ﻿
 using Core.Entities;
 using Core.Interfaces;
+using StackExchange.Redis;
+using System;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Data;
 
 public class BasketRepository : IBasketRepository
 {
-    public Task<bool> DeleteBasketAsync(string basketId)
+    private readonly IDatabase _database;
+
+    public BasketRepository(IConnectionMultiplexer redis)
     {
-        throw new System.NotImplementedException();
+        this._database = redis.GetDatabase();
     }
 
-    public Task<CustomerBasket> GetBasketAsync(string basketId)
+    public async Task<bool> DeleteBasketAsync(string basketId)
     {
-        throw new System.NotImplementedException();
+        return await this._database.KeyDeleteAsync(basketId);
     }
 
-    public Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+    public async Task<CustomerBasket> GetBasketAsync(string basketId)
     {
-        throw new System.NotImplementedException();
+        var data = await this._database.StringGetAsync(basketId);
+
+        var result = data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<CustomerBasket>(data);
+
+        return result;
+    }
+
+    public async Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+    {
+        var created = await this._database
+                                .StringSetAsync(basket.Id, JsonSerializer.Serialize(basket), TimeSpan.FromDays(30));
+
+        if (!created) return null;
+
+        return await GetBasketAsync(basket.Id);
     }
 }
