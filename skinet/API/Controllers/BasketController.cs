b50c8596d4748f7e99d61a9e1c68fbfb407316ca
mithun-Das <mithun.das227@gmail.com>
2023-01-24 23:38:22 +0600

    Section-13: Part-140 -> Adding the basket controller

---
+++ skinet/API/Controllers/BasketController.cs
@@ -0,0 +1,38 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers;
+
+public class BasketController : BaseApiController
+{
+    private readonly IBasketRepository _basketRepository;
+
+    public BasketController(IBasketRepository basketRepository)
+    {
+        this._basketRepository = basketRepository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
+    {
+        var data = await this._basketRepository.GetBasketAsync(id);
+
+        return Ok(data ?? new CustomerBasket(id));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CustomerBasket>> UpdateBasket([FromBody] CustomerBasket customerBasket)
+    {
+        var updateBasket = await this._basketRepository.UpdateBasketAsync(customerBasket);
+
+        return Ok(updateBasket);
+    }
+
+    [HttpDelete]
+    public async Task DeleteBasket(string id)
+    {
+        await this._basketRepository.DeleteBasketAsync(id);
+    }
+}
