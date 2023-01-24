7c56adbf60a1cea27dc191e61bde44f7cd6180f4
mithun-Das <mithun.das227@gmail.com>
2023-01-24 18:14:24 +0600

    Section-13: Part-137 -> Setting up the basket class

---
+++ skinet/Core/Entities/CustomerBasket.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Generic;
+
+namespace Core.Entities;
+
+public class CustomerBasket
+{
+    public CustomerBasket() { }
+
+    public CustomerBasket(string id)
+    {
+        this.Id = id;
+    }
+
+    public string Id { get; set; }
+    public List<BasketItem> BasketItems { get; set; } = new List<BasketItem>();
+}
