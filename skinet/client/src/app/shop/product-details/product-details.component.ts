5da5e1c7044254cdcf8daffd27de2137d61dfb19
mithun-Das <mithun.das227@gmail.com>
2023-01-27 17:45:24 +0600

    Section-14: Part-159 -> Hooking up the product detail component to the basket

--- skinet/client/src/app/shop/product-details/product-details.component.ts
+++ skinet/client/src/app/shop/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { BreadcrumbService } from 'xng-breadcrumb';
+import { BasketService } from 'src/app/basket/basket.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,11 +12,13 @@ import { BreadcrumbService } from 'xng-breadcrumb';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
+  quantity = 1;
 
   constructor(
     private shopService: ShopService,
     private activateRoute: ActivatedRoute,
-    private bcService: BreadcrumbService
+    private bcService: BreadcrumbService,
+    private basketService: BasketService
   ) {
     this.bcService.set('@productDetails', ' ');
   }
@@ -30,11 +33,25 @@ export class ProductDetailsComponent implements OnInit {
       .subscribe(
         (response) => {
           this.product = response;
-          this.bcService.set('@productDetails', this.product.name)
+          this.bcService.set('@productDetails', this.product.name);
         },
         (error) => {
           console.log(error);
         }
       );
+  }
+
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
   }
 }
