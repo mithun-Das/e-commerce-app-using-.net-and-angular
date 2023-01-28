ebcf3fcffd51e5c2fb1daddcb39676b191778fe4
mithun-Das <mithun.das227@gmail.com>
2023-01-28 22:23:40 +0600

    Section-15: Part-170 -> Adding an Account controller

---
+++ skinet/API/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using API.Dtos;
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using API.Errors;
+
+namespace API.Controllers;
+
+public class AccountController : BaseApiController
+{
+    private readonly UserManager<AppUser> _userManager;
+    private readonly SignInManager<AppUser> _signInManager;
+
+
+    public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var user = await this._userManager.FindByEmailAsync(loginDto.Email);
+
+        if (user == null) { return Unauthorized(new ApiResponse(401)); }
+
+        var result = await this._signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+        if(!result.Succeeded) { return Unauthorized(new ApiResponse(401)); }
+
+        return new UserDto
+        {
+            Email = user.Email,
+            DisplayName = user.DisplayName,
+            Token = "This is Token"
+        };
+    }
+}
