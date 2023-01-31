46c81318c3d4c641ef367271e90d88bfd6fbc19d
mithun-Das <mithun.das227@gmail.com>
2023-02-01 00:11:44 +0600

    Section-17: Part-191 -> Using reactive forms in Angular

--- skinet/client/src/app/account/login/login.component.ts
+++ skinet/client/src/app/account/login/login.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(){
+    console.log(this.loginForm.value);
   }
-
 }
