a87151466acb3f9ba76300bbd5d37ab35ae9e28d
mithun-Das <mithun.das227@gmail.com>
2023-02-01 11:25:11 +0600

    Section-17: Part-193 -> Updating the nav bar for the logged in user

--- skinet/client/src/app/core/nav-bar/nav-bar.component.ts
+++ skinet/client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AccountService } from 'src/app/account/account.service';
 import { BasketService } from 'src/app/basket/basket.service';
 import { IBasket } from 'src/app/shared/models/basket';
 
@@ -11,7 +12,10 @@ import { IBasket } from 'src/app/shared/models/basket';
 export class NavBarComponent implements OnInit {
   basket$: Observable<IBasket>;
 
-  constructor(private basketService: BasketService) {}
+  constructor(
+    private basketService: BasketService,
+    public accountService: AccountService
+  ) { }
 
   ngOnInit(): void {
     this.basket$ = this.basketService.basket$;
