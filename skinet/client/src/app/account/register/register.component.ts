cc63ab9f3d65571393ef4b57c72f6c63380e817e
mithun-Das <mithun.das227@gmail.com>
2023-02-02 13:58:28 +0600

    Section-17: Part-202 -> Async validation

--- skinet/client/src/app/account/register/register.component.ts
+++ skinet/client/src/app/account/register/register.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { AsyncValidatorFn, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AccountService } from '../account.service';
+import { finalize, map } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -22,7 +23,7 @@ export class RegisterComponent {
 
   registerForm = this.fb.group({
     displayName: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
+    email: ['', [Validators.required, Validators.email], [this.validateEmailNotTaken()]],
     password: [
       '',
       [Validators.required, Validators.pattern(this.complexPasswordPattern)],
@@ -35,4 +36,16 @@ export class RegisterComponent {
       error: (error) => this.errors = error.errors
     });
   }
+
+  validateEmailNotTaken() : AsyncValidatorFn {
+
+    return (control: AbstractControl) => {
+      return this.accountService.checkEmailExists(control.value).pipe(
+        map((result) => result ? {emailExists: true} : null),
+        finalize(() => control.markAsTouched())
+      )
+    }
+
+  }
+
 }
