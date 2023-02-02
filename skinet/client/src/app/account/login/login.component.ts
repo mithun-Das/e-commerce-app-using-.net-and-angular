dfc3c9aea08155466f6aec296bc7df9917d05e27
mithun-Das <mithun.das227@gmail.com>
2023-02-02 16:56:17 +0600

    Section-17: Part-204 -> Creating an auth guard

--- skinet/client/src/app/account/login/login.component.ts
+++ skinet/client/src/app/account/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AccountService } from '../account.service';
 
 @Component({
@@ -9,18 +9,26 @@ import { AccountService } from '../account.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private accountService: AccountService, private router: Router) {}
+  constructor(
+    private accountService: AccountService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] || '/shop';
+  }
 
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required),
   });
 
+  returnUrl: string;
+
   ngOnInit(): void {}
 
-  onSubmit(){
+  onSubmit() {
     this.accountService.login(this.loginForm.value).subscribe({
-      next: () => this.router.navigateByUrl("/shop")
+      next: () => this.router.navigateByUrl(this.returnUrl),
     });
   }
 }
--- skinet/client/src/app/app-routing.module.ts
+++ skinet/client/src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', 
-    component: HomeComponent, 
-    data: { breadcrumb: 'Home' } 
-  },
+  { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
   {
     path: 'test-error',
     component: TestErrorComponent,
@@ -39,6 +37,7 @@ const routes: Routes = [
   },
   {
     path: 'checkout',
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./checkout/checkout.module').then((mod) => mod.CheckoutModule),
     data: { breadcrumb: 'Checkout' },
