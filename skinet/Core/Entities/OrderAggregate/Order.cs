088ea43605c6f5289def924731bc07e6018d0d2d
mithun-Das <mithun.das227@gmail.com>
2023-02-05 21:04:25 +0600

    Section-18: Part-209 -> Creating the order aggregate part 2

---
+++ skinet/Core/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Core.Entities.OrderAggregate;
+
+public class Order : BaseEntity
+{
+    public Order() { }
+
+    public Order(string buyerEmail, Address shipToAddress, DeliveryMethod deliveryMethod,
+        IReadOnlyList<OrderItem> orderItems, decimal subTotal)
+    {
+        this.BuyerEmail = buyerEmail;
+        this.ShipToAddres = shipToAddress;
+        this.DeliveryMethod = deliveryMethod;
+        this.OrderItems = orderItems;
+        this.SubTotal = subTotal;
+    }
+
+    public string BuyerEmail { get; set; }
+    public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+    public Address ShipToAddres { get; set; }
+    public DeliveryMethod DeliveryMethod { get; set; }
+    public IReadOnlyList<OrderItem> OrderItems { get; set; }
+    public decimal SubTotal { get; set; }
+    public OrderStatus Status { get; set; }
+    public string PaymentIntentId { get; set; }
+    public decimal GetTotal()
+    {
+        return SubTotal + DeliveryMethod.Price;
+    }
+}
