0a730d0908a54223d0b5ab9bb2959d32e5db9d08
mithun-Das <mithun.das227@gmail.com>
2023-02-06 17:14:36 +0600

    Section-18: Part-215 -> Creating the order controller

---
+++ skinet/API/Controllers/OrdersController.cs
@@ -0,0 +1,40 @@
+﻿
+using API.Dtos;
+using API.Errors;
+using API.Extensions;
+using AutoMapper;
+using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers;
+
+[Authorize]
+public class OrdersController : BaseApiController
+{
+    private readonly IOrderService _orderService;
+    private readonly IMapper _mapper;
+
+    public OrdersController(OrderService orderService, IMapper mapper)
+    {
+        this._orderService = orderService;
+        this._mapper = mapper;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Order>> CreateOrder(OrderDto orderDto)
+    {
+        var email = HttpContext.User.RetrieveEmailFromPrincipal();
+
+        var address = this._mapper.Map<AddressDto, Address>(orderDto.ShipToAddress);
+
+        var order = await this._orderService.CreateOrderAsync(email, orderDto.DeliveryMethodId, orderDto.BasketId, address);
+
+        if (order == null) return BadRequest(new ApiResponse(400, "Problem creating order"));
+
+        return Ok(order);
+    }
+}
