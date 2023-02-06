ee59db7ca9fdce4836f2c5f23fb7795fbef01057
mithun-Das <mithun.das227@gmail.com>
2023-02-07 00:18:45 +0600

    Section-18: Part-218 -> Implementing the Unit of work

---
+++ skinet/Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿
+using Core.Entities;
+using Core.Interfaces;
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly StoreContext _context;
+    private Hashtable _repositories;
+
+    public UnitOfWork(StoreContext context) 
+    {
+        _context = context;
+    }
+
+    public async Task<int> Complete()
+    {
+        return await this._context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        this._context.Dispose();
+    }
+
+    public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity
+    {
+        if(this._repositories == null) this._repositories = new Hashtable();
+
+        var type = typeof(TEntity).Name;
+
+        if (!this._repositories.ContainsKey(type))
+        {
+            var repositoryType = typeof(GenericRepository<>);
+            var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(TEntity)), _context);
+
+            this._repositories.Add(type, repositoryInstance);
+        }
+
+        return (IGenericRepository<TEntity>) this._repositories[type];
+    }
+}
