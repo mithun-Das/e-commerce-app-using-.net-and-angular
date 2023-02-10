5b64e4a93872fec3a4bc738e71cdcf6bfbf70435
mithun-Das <mithun.das227@gmail.com>
2023-02-10 17:54:51 +0600

    Section-19: Part-234 -> Adding the checkout form

--- skinet/client/src/app/checkout/checkout.component.ts
+++ skinet/client/src/app/checkout/checkout.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  checkOutForm = this.fb.group({
+    addressForm: this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zipcode: ['', Validators.required],
+    }),
+    deliveryForm: this.fb.group({
+      deliveryMethod: ['', Validators.required],
+    }),
+    paymentForm: this.fb.group({
+      nameOnCard: ['', Validators.required],
+    }),
+  });
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
