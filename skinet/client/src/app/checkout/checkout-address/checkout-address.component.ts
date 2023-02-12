82a5a18602625b2031f7419b2798d47340c8346a
mithun-Das <mithun.das227@gmail.com>
2023-02-13 00:45:34 +0600

    Section-19: Part-244 -> Letting the user save the address

--- skinet/client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ skinet/client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from 'src/app/account/account.service';
 
 @Component({
   selector: 'app-checkout-address',
   templateUrl: './checkout-address.component.html',
-  styleUrls: ['./checkout-address.component.scss']
+  styleUrls: ['./checkout-address.component.scss'],
 })
 export class CheckoutAddressComponent implements OnInit {
-
   @Input() checkoutForm?: FormGroup;
 
-  constructor() { }
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  saveUserAddress() {
+    this.accountService
+      .updateUserAddress(this.checkoutForm.get('addressForm')?.value)
+      .subscribe({
+        next: () => {
+          this.toastr.show('User Address Updated');
+          this.checkoutForm
+            .get('addressForm')
+            ?.reset(this.checkoutForm.get('addressForm').value);
+        },
+      });
   }
-
 }
