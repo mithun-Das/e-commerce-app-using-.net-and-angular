8d8c648dfe48a95db99c1bc3ff8f8d24206aef6a
mithun-Das <mithun.das227@gmail.com>
2023-02-13 15:55:22 +0600

    Section-19: Part-247 -> Order submission

--- skinet/client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ skinet/client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { BasketService } from 'src/app/basket/basket.service';
+import { Basket } from 'src/app/shared/models/basket';
+import { Address } from 'src/app/shared/models/user';
+import { CheckoutService } from '../checkout.service';
 
 @Component({
   selector: 'app-checkout-payment',
   templateUrl: './checkout-payment.component.html',
-  styleUrls: ['./checkout-payment.component.scss']
+  styleUrls: ['./checkout-payment.component.scss'],
 })
 export class CheckoutPaymentComponent implements OnInit {
+  @Input() checkoutForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private basketService: BasketService,
+    private checkoutService: CheckoutService,
+    private toastr: ToastrService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submitOrder() {
+    const basket = this.basketService.getCurrentBasketValue();
+    if (!basket) return;
+
+    const orderToCreate = this.getOrderToCreate(basket);
+    if (!orderToCreate) return;
+
+    this.checkoutService.createOrder(orderToCreate).subscribe({
+      next: (order) => {
+        this.toastr.show('Order created successfully');
+        console.log(order);
+      },
+    });
   }
 
+  private getOrderToCreate(basket: Basket) {
+    const deliveryMethodId = this.checkoutForm
+      .get('deliveryForm')
+      ?.get('deliveryMethod')?.value;
+    const shipToAddress = this.checkoutForm.get('addressForm')
+      ?.value as Address;
+
+    if (!deliveryMethodId || !shipToAddress) return;
+
+    return {
+      basketId: basket.id,
+      deliveryMethodId: deliveryMethodId,
+      shipToAddress: shipToAddress,
+    };
+  }
 }
