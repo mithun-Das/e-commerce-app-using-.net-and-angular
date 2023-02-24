3f3d4db082671c436b39f709694af77250d51ea3
mithun-Das <mithun.das227@gmail.com>
2023-02-24 18:57:12 +0600

    Section-21: Part-264 -> Using the app stepper as an input property

--- skinet/client/src/app/checkout/checkout-review/checkout-review.component.ts
+++ skinet/client/src/app/checkout/checkout-review/checkout-review.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkStepper } from '@angular/cdk/stepper';
+import { Component, OnInit, Input } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { BasketService } from 'src/app/basket/basket.service';
 
@@ -8,6 +9,9 @@ import { BasketService } from 'src/app/basket/basket.service';
   styleUrls: ['./checkout-review.component.scss'],
 })
 export class CheckoutReviewComponent implements OnInit {
+
+  @Input() appStepper?: CdkStepper;
+
   constructor(
     private basketService: BasketService,
     private toastr: ToastrService
@@ -17,7 +21,10 @@ export class CheckoutReviewComponent implements OnInit {
 
   createPaymentIntent() {
     this.basketService.createPaymentIntent().subscribe({
-      next: (response) => this.toastr.show('Payment Intent Created'),
+      next: (response) => {
+        this.toastr.show('Payment Intent Created'),
+        this.appStepper?.next();
+      },
       error: (error) => console.log(error),
     });
   }
