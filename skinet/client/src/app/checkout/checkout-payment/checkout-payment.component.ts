485689a960ec2ff087542488d4f7b91752233048
mithun-Das <mithun.das227@gmail.com>
2023-02-24 15:32:30 +0600

    Section-21: Part-262 -> Adding stripe elements to the components

--- skinet/client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ skinet/client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { NavigationExtras, Router } from '@angular/router';
+import { Stripe } from '@stripe/stripe-js';
+import {
+  StripeCardCvcElement,
+  StripeCardExpiryElement,
+  StripeCardNumberElement,
+} from '@stripe/stripe-js/types/stripe-js';
 import { ToastrService } from 'ngx-toastr';
 import { BasketService } from 'src/app/basket/basket.service';
 import { Basket } from 'src/app/shared/models/basket';
 import { Address } from 'src/app/shared/models/user';
 import { CheckoutService } from '../checkout.service';
+import { loadStripe } from '@stripe/stripe-js';
 
 @Component({
   selector: 'app-checkout-payment',
@@ -14,6 +21,15 @@ import { CheckoutService } from '../checkout.service';
 })
 export class CheckoutPaymentComponent implements OnInit {
   @Input() checkoutForm: FormGroup;
+  @ViewChild('cardNumber') cardNumberElement: ElementRef;
+  @ViewChild('cardExpiry') cardExpiryElement: ElementRef;
+  @ViewChild('cardCvc') cardCvcElement: ElementRef;
+  stripe: Stripe | null = null;
+  cardNumber?: StripeCardNumberElement;
+  cardExpiry?: StripeCardExpiryElement;
+  cardCvc?: StripeCardCvcElement;
+  publishableKey =
+    'pk_test_51Mc0PxIfd8QYIl1PO0RCR2b2v8n4KFEdFUmjzZh1h1DWAr3xCLQCOpCA8d8eMylRvibFxnfCKKcNmUqP1k9FPrEI00GbbVDlVa';
 
   constructor(
     private basketService: BasketService,
@@ -22,7 +38,23 @@ export class CheckoutPaymentComponent implements OnInit {
     private router: Router
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    loadStripe(this.publishableKey).then((stripe) => {
+      this.stripe = stripe;
+      console.log(this.stripe);
+      const elements = this.stripe?.elements();
+      if (elements) {
+        this.cardNumber = elements.create('cardNumber');
+        this.cardNumber.mount(this.cardNumberElement?.nativeElement);
+
+        this.cardExpiry = elements.create('cardExpiry');
+        this.cardExpiry.mount(this.cardExpiryElement?.nativeElement);
+
+        this.cardCvc = elements.create('cardCvc');
+        this.cardCvc.mount(this.cardCvcElement?.nativeElement);
+      }
+    });
+  }
 
   submitOrder() {
     const basket = this.basketService.getCurrentBasketValue();
