f788b091cb71f213b4da44a3cc9533def9d506a3
mithun-Das <mithun.das227@gmail.com>
2023-03-05 17:48:33 +0600

    Section-22: Part-280 -> Caching on the client part 1

--- skinet/client/src/app/shop/shop.service.ts
+++ skinet/client/src/app/shop/shop.service.ts
@@ -3,21 +3,22 @@ import { Injectable } from '@angular/core';
 import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
-import { map } from 'rxjs';
+import { map, of } from 'rxjs';
 import { ShopParams } from '../shared/models/shopParams';
 import { IProduct } from '../shared/models/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShopService {
+  baseUrl = 'https://localhost:5001/api/';
+  products: IProduct[] = [];
+  brands: IBrand[] = [];
+  types: IType[] = [];
 
-  baseUrl = "https://localhost:5001/api/";
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getProducts(shopParams: ShopParams) {
-
     let params = new HttpParams();
 
     if (shopParams.brandId !== 0) {
@@ -28,7 +29,7 @@ export class ShopService {
       params = params.append('typeId', shopParams.typeId.toString());
     }
 
-    if(shopParams.search) {
+    if (shopParams.search) {
       params = params.append('search', shopParams.search);
     }
 
@@ -36,24 +37,46 @@ export class ShopService {
     params = params.append('pageIndex', shopParams.pageNumber.toString());
     params = params.append('pageSize', shopParams.pageSize.toString());
 
-    return this.http.get<IPagination>(this.baseUrl + 'products', { observe: 'response', params })
+    return this.http
+      .get<IPagination>(this.baseUrl + 'products', {
+        observe: 'response',
+        params,
+      })
       .pipe(
-        map(response => {
+        map((response) => {
+          this.products = response.body.data;
           return response.body;
         })
       );
   }
 
   getProduct(id: number) {
+    const product = this.products.find((x) => x.id == id);
+
+    if (product != null) return of(product);
+
     return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
-    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+    if (this.brands.length > 0) return of(this.brands);
+
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands').pipe(
+      map((response) => {
+        this.brands = response;
+        return response;
+      })
+    );
   }
 
   getTypes() {
-    return this.http.get<IType[]>(this.baseUrl + 'products/types');
-  }
+    if (this.types.length > 0) return of(this.types);
 
+    return this.http.get<IType[]>(this.baseUrl + 'products/types').pipe(
+      map((response) => {
+        this.types = response;
+        return response;
+      })
+    );
+  }
 }
